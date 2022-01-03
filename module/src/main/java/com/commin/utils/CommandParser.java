22390913950dbe6b7e32074ccde109a6ced0f9d5
khm0813 <khm.commin@gmail.com>
2022-01-03 09:19:21 +0900

    parser modified

--- module/src/main/java/com/commin/utils/CommandParser.java
+++ module/src/main/java/com/commin/utils/CommandParser.java
@@ -1,5 +1,6 @@
 package com.commin.utils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -13,10 +14,15 @@ public class CommandParser {
         if (cmd == null)
             throw new NullPointerException();
 
-        List<String> command_result_arr = argMap.keySet().stream().map(key -> {
+        List<String> command_result_arr = new ArrayList<>();
+        command_result_arr.add(0, cmd);
+
+        List<String> command_args = argMap.keySet().stream().map(key -> {
             return String.format("%s %s", key, argMap.get(key));
         }).collect(Collectors.toList());
-        command_result_arr.add(0, cmd);
+
+        if (command_args != null && !command_args.isEmpty())
+            command_result_arr.addAll(command_args);
 
         return String.join(" ", command_result_arr);
     }
